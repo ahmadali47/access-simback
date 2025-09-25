b6e173d31d1610357f14ec52068123e4d70f459f
Ahmad Ali <ahmad.aa47ali@gmail.com>
2025-09-26 03:49:56 +0500

    just dleting

--- back/models/Employee.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-export default mongoose.model("Employee", new mongoose.Schema({
-  id: String,
-  access_level: Number,
-  request_time: String,
-  room: String
-}));
